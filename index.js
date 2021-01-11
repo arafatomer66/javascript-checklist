9324efae732ae327fb2cc009419b98c4c86e35a8
Md Omer Arafat <arafatomer66@gmail.com>
2021-01-11 15:41:21 +0600

    js data types

---
+++ index.js
@@ -0,0 +1,7 @@
+//There are 2 types of data types
+
+//1.Premitive data type ---stored directly
+// string , number , bool . null , undefined 
+
+//2. Non premitive--- reference type
+//array , object , functions
